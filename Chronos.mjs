660226328162c75c9e046b550cebcaa469ad053a
R1shabh-Gupta <imailrishabhgupta@gmail.com>
2024-08-19 14:36:13 +0530

    feat: add updateStagingArea and commit methods to Chronos class

--- Chronos.mjs
+++ Chronos.mjs
@@ -32,9 +32,47 @@ class Chronos {
     console.log(fileHash);
     const newFileHashedObjectPath = path.join(this.objectsPath, fileHash);
     await fs.writeFile(newFileHashedObjectPath, fileData);
+    await this.updateStagingArea(fileToBeAdded, fileHash);
     console.log(`Added ${fileToBeAdded} to the index`);
+  }
+
+  async updateStagingArea(filePath, fileHash) {
+    const index = JSON.parse(await fs.readFile(this.indexPath, "utf-8"));
+    index.push({ path: filePath, hash: fileHash });
+    await fs.writeFile(this.indexPath, JSON.stringify(index));
+  }
+
+  async commit(message) {
+    const index = JSON.parse(await fs.readFile(this.indexPath, "utf-8"));
+    const parentCommit = await this.getCurrentHead();
+
+    const commitData = {
+      timeStamp: new Date().toISOString(),
+      message,
+      files: index,
+      parent: parentCommit,
+    };
+
+    const commitHash = this.hashObject(JSON.stringify(commitData));
+    const commitPath = path.join(this.objectsPath, commitHash);
+    await fs.writeFile(commitPath, JSON.stringify(commitData));
+    await fs.writeFile(this.headPath, commitHash);
+    await fs.writeFile(this.indexPath, JSON.stringify([]));
+    console.log(`Committed sucessfully created: ${commitHash}`);
+  }
+
+  async getCurrentHead() {
+    try {
+      return await fs.readFile(this.headPath, "utf-8");
+    } catch (error) {
+      console.log("No commits yet");
+      return null;
+    }
   }
 }
 
-const chronos = new Chronos();
-chronos.add("sample.txt");
+(async () => {
+  const chronos = new Chronos();
+  await chronos.add("sample.txt");
+  await chronos.commit("Initial commit");
+})();
